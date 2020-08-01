cff2196745f0bf9dec7a6da77ebfb717651e57a4
Dainn <vijay.s.venkatesan@protonmail.com>
2020-08-01 14:34:30 -0400

    Added timing with chrono

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # fizzbuzz
 
-This program tests different ways to solve the fizzbuzz problem. The primary metric is computation time which is calculated using timers from time.h. 
+This program tests different ways to solve the fizzbuzz problem. The primary metric is computation time which is calculated using timers from chrono. 
--- fizzbuzz.cpp
+++ fizzbuzz.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <chrono>
 
 using namespace std;
 
@@ -9,6 +10,7 @@ int main(int argc, char * argv []){
 	if(argc != 2){ return 1; }
 	int n = stoi(argv[1]);
 	
+	auto startTime = chrono::steady_clock::now();
 	// Not elegant, but it works
 	for(int i = 1; i <= n; i++){
 	       if(i % 3 == 0 && i % 5 == 0){ cout << "Fizz Buzz" << endl; }
@@ -16,4 +18,8 @@ int main(int argc, char * argv []){
 	       else if(i % 3 == 0){ cout << "Fizz" << endl; }
 	       else{ cout << i << endl;}
 	}
+	auto endTime = chrono::steady_clock::now();
+	cout << endl << "Method 1 completed in: " 
+		<< chrono::duration_cast<chrono::microseconds>(endTime - startTime).count() 
+		<< "µs" << endl << endl;
 }
